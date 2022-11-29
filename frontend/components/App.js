7ac03fad559123abb75455c4467d3f0d88394f7d
Robby Lewis <Robbysim37@gmail.com>
2022-11-29 17:19:06 -0500

    class components suck

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import TodoList from "./TodoList"
+import Form from "./Form"
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      todoBuilder: {name:"",id:0,completed:false},
+      todoList: [],
+      test:"test"
+    }
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    this.setState({todoList: [...this.state.todoList,this.state.name]})
+    console.log(this.state.todoList)
+  }
+
+  onInputChange = (e) => {
+    this.setState({name: e.target.value})
+    console.log(this.state.name)
+  }
+
   render() {
     return (
       <div>
-        Todo App
+        <TodoList todoList={this.state.todoList} test={this.state.test}></TodoList>
+        <Form submitHandler={this.submitHandler} inputChangeHandler={this.onInputChange}/>
       </div>
     )
   }
--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -2,9 +2,14 @@ import React from 'react'
 
 export default class Todo extends React.Component {
   render() {
+
+      // itemClickHandler = () => {
+      //   //runs whatever function we pass down
+      //   //remember to add the 'onClick' to the div
+      // }
     return (
       <div>
-        Todo
+        {this.props.item}
       </div>
     )
   }
